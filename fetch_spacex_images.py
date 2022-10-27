8d4ee0b4c11c70f976c6853d7c21788856f55263
EvgenyM <MelnikovEI@ya.ru>
2022-10-27 20:32:31 +0300

    Resolve names conflict

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,9 +5,9 @@ import requests
 from images_api import load_image, get_ext
 
 
-def fetch_spacex_launch(id: str = ''):
-    if id:
-        space_x_response = requests.get(f"https://api.spacexdata.com/v5/launches/{id}")
+def fetch_spacex_launch(launch_id: str = ''):
+    if launch_id:
+        space_x_response = requests.get(f"https://api.spacexdata.com/v5/launches/{launch_id}")
     else:
         space_x_response = requests.get("https://api.spacexdata.com/v5/launches/latest")
     space_x_response.raise_for_status()
@@ -23,6 +23,6 @@ def fetch_spacex_launch(id: str = ''):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='downloads the latest image of SpaceX launch to ".images" folder.')
-    parser.add_argument("id", nargs='?', help='id of specific launch')
+    parser.add_argument("id", nargs='?', help='launch_id of specific launch')
     args = parser.parse_args()
     print(fetch_spacex_launch(args.id))
