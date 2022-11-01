e84600069463c4a4bb1c0637cfd6df4212437992
EvgenyM <MelnikovEI@ya.ru>
2022-11-01 19:06:06 +0300

    Exclude if/else using default value

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,11 +5,8 @@ import requests
 from images_api import load_image, get_ext
 
 
-def fetch_spacex_launch(launch_id: str = ''):
-    if launch_id:
-        space_x_response = requests.get(f"https://api.spacexdata.com/v5/launches/{launch_id}")
-    else:
-        space_x_response = requests.get("https://api.spacexdata.com/v5/launches/latest")
+def fetch_spacex_launch(launch_id: str = 'latest'):
+    space_x_response = requests.get(f"https://api.spacexdata.com/v5/launches/{launch_id}")
     space_x_response.raise_for_status()
     picture_links = space_x_response.json()['links']['flickr']['original']
     if picture_links:
@@ -23,7 +20,7 @@ def fetch_spacex_launch(launch_id: str = ''):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='downloads the latest image of SpaceX launch to ".images" folder.')
-    parser.add_argument("id", nargs='?', help='id of specific launch')
+    parser.add_argument("id", nargs='?', help='id of specific launch', default='latest')
     args = parser.parse_args()
     if fetch_spacex_launch(args.id) is None:
         print("Process failed: Server didn't return expected information for downloading images")
