4cec01c7e889d3d5a3d5a6fa753d14fe3e51b90b
EvgenyM <MelnikovEI@ya.ru>
2022-11-03 20:39:22 +0300

    imporved var name

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -14,8 +14,8 @@ def fetch_nasa_epic_image(token: str, date: str = ''):
     }
     nasa_response = requests.get(f"https://api.nasa.gov/EPIC/api/natural/date/{date}", params=params)
     nasa_response.raise_for_status()
-    nasa_response_json = nasa_response.json()
-    for i, img_item in enumerate(nasa_response_json):
+    nasa_response_info = nasa_response.json()
+    for i, img_item in enumerate(nasa_response_info):
         img_name = img_item['image']
         if img_name:
             img_date = datetime.datetime.strptime(img_item.get('date'), '%Y-%m-%d %H:%M:%S').date()
